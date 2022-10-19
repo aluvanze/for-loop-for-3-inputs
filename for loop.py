dd43e738b953b997fb036e8a4a8b27db5a76a305
Jacob Aluvanze <77957856+aluvanze@users.noreply.github.com>
2022-10-19 20:46:26 +0300

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,16 @@
+x = int(input("input math\n"))
+for i in range(1):
+    y = int(input("input english\n"))
+    z = int(input("input kiswahili\n"))
+
+    sum=x+y+z
+    average=(x+y+z)/3
+    while average > 8:
+        print("Good Work")
+
+        break
+    else:
+        print("try harder next time")
+
+    print("Sum",sum)
+    print("average", average)
