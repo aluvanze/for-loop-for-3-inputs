dc436c77b9b58978e7937883ffe8c622cc42f89e
Jacob Aluvanze <77957856+aluvanze@users.noreply.github.com>
2022-10-20 10:55:46 +0300

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -1,9 +1,30 @@
-for i in range(1):
-    y = int(input("input english\n"))
-    z = int(input("input kiswahili\n"))
-    x = int(input("input math\n"))
-    sum=x+y+z
-    average=(x+y+z)/3
+subjects=3
+
+for i in range(subjects):
+
+
+    x = int(input("input english\n"))
+
+    i=i+1
+
+    if i==1:
+        y = int(input("input math\n"))
+    i=i+1
+
+    if i==2:
+            z = int(input("input kiswahili\n"))
+            i=i+1
+
+    if i == 3:
+        a = int(input("input sheng\n"))
+        i=0+1
+
+
+
+
+
+    sum = x + y + z + a
+    average = (x + y + z + a) / 4
     while average > 8:
         print("Good Work")
 
@@ -11,5 +32,5 @@ for i in range(1):
     else:
         print("try harder next time")
 
-    print("Sum",sum)
+    print("Sum", sum)
     print("average", average)
